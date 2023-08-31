3501d9ae51cff921a72da0d6fc60b8d86abd84a9
will <w.duffy@outlook.com>
2023-08-30 20:45:13 -0500

    added additional practices we completed on 8-30

--- 1-Module/2-week/3-day/scratch.js
+++ 1-Module/2-week/3-day/scratch.js
@@ -41,31 +41,31 @@ product of all numbers multiplied together.
 */
 // let arr = [1,2 ]
 
-// let multiplierFunc = function (arr){
-//     let product = 1;
+let multiplierFunc = function (arr){
+    let product = 1;
 
-//     for (let i = 0; i < arr.length;  i++){
-//         product *= arr[i]
-//     }
-//     return product
-// }
-// // console.log(multiplierFunc([6, 4]))
-// // console.log(multiplierFunc([5]))
-// // console.log(multiplierFunc([3, 1]))
-// // console.log("HERE", product)
+    for (let i = 0; i < arr.length;  i++){
+        product *= arr[i]
+    }
+    return product
+}
+// console.log(multiplierFunc([6, 4]))
+// console.log(multiplierFunc([5]))
+// console.log(multiplierFunc([3, 1]))
+// console.log("HERE", product)
 
-// let twoDimensionalProduct = function (twoDimArray){
+let twoDimensionalProduct = function (twoDimArray){
 
-//     let productTotal = 1;
+    let productTotal = 1;
 
-//     for (let i = 0; i < twoDimArray.length;  i++){
-//         productTotal *= multiplierFunc(twoDimArray[i])
-//     }
-//     //return total of all nested elements mulitplied together
+    for (let i = 0; i < twoDimArray.length;  i++){
+        productTotal *= multiplierFunc(twoDimArray[i])
+    }
+    //return total of all nested elements mulitplied together
 
-//     return productTotal
+    return productTotal
 
-// }
+}
 
 // let arr1 = [
 //     [6, 4],
@@ -150,6 +150,62 @@ let longestWord = function(sentence) {
 
 //an update
 
-console.log(longestWord('where did everyone go')); // 'everyone' 2
+// console.log(longestWord('where did everyone go')); // 'everyone' 2
 // console.log(longestWord('prefer simplicity over complexity')); // 'simplicity' 1
 // console.log(longestWord('')); // ''
+/*
+Uncompress
+Write a function uncompress(str) that takes in a "compressed" string as an arg.
+A compressed string consists of a character immediately followed
+by the number of times it appears in the "uncompressed" form.
+The function should return the uncompressed version of the string. See the examples.
+
+Hint: you can use the built-in Number function should convert a numeric string into the number type.
+// For example. Number("4") // => 4
+*/
+
+
+let uncompress = function (str) {
+
+    let answerStr = "";
+
+let numArr = []
+let strArr = []
+    //Add to answer to string += "somestr"
+for (let i = 0; i < str.length; i+=2) {
+    let letter = str[i]
+    strArr.push(letter)
+    // let num = str[i + 1]
+    // console.log("MY FLAG", letter)
+}
+
+for (let i = 1; i < str.length; i+=2){
+    let num = str[i]
+    // console.log("NUM", num)
+    numArr.push(num)
+    // let num = str[i + 1]
+    // console.log(letter, num)
+}
+console.log(numArr, strArr)
+
+
+for(let i =0; i< numArr.length; i++){
+    let finalNum = Number(numArr[i])
+    // console.log(finalNum)
+    let finalLetter = strArr[i]
+    // console.log(finalNum, finalLetter)
+    for (let j = 0; j < finalNum; j++){
+        // console.log(finalLetter)
+        answerStr+= finalLetter
+    }
+}
+// console.log(numArr, strArr)
+//return the uncompresssed v string
+return answerStr
+}
+
+
+
+console.log(uncompress('x3y4z2')); // 'xxxyyyyzz'
+console.log(uncompress('a5b2c4z1')); // 'aaaaabbccccz'
+console.log(uncompress('b1o2t1')); // 'boot'
