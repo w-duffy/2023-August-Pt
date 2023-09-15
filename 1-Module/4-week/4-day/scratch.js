6e8cc56a8580be323344dfbd290e6e4cac3b1ef1
will <w.duffy@outlook.com>
2023-09-15 02:18:42 -0500

    updated scratch for Thursday

--- 1-Module/4-week/4-day/scratch.js
+++ 1-Module/4-week/4-day/scratch.js
@@ -1,37 +1,16 @@
 /*
 Elden Ring Weapon Damage Boost
 
-In Elden Ring, weapons that haven't seen extensive use are sharper and more effective.
-The game developers have decided to grant a 20% damage boost to weapons that
-have been used for less than 500 battles.
+The Elden Ring developers decide that weapons that haven't seen extensive use
+should be made more effective. The game developers have decided to grant a 20%
+damage boost to weapons that have been used for less than 500 battles.
 
 Write a function boostedWeapons(weaponsArray) that takes an array of objects containing
 weapon names and their usage counts in battles. The function should return an array containing
 the weapons that qualify for the damage boost, along with their new boosted damage.
 
-Try solving this without modifying the original array && its elements should not be modified.
+Bonus: Try solving this without modifying the original array && its elements should not be modified.
 */
-
-
-//
-let boostedWeapons = (weaponsArray) => {
-    let answer = filterArr.map(el => {
-
-        if(el.includes('weapon')){
-
-          return el
-
-        }
-
-        if(el.includes('bonusDamage')){
-
-          return el
-
-        }
-
-      })
-
-}
 
 
 weapons = [
@@ -50,9 +29,10 @@ weapons = [
   //   { weapon: 'Starlight Bow', bonusDamage: 144 }
   // ]
 
-  console.log(weapons);
-  //PRINTS:
-//   [
+//   BONUS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//     Original Array Not Modified
+  //   console.log(weapons) // Prints:
+//  [
 //     { weapon: "Moonlight Greatsword", battles: 600, currDamage: 150 },
 //     { weapon: "Lunar Scythe", battles: 480, currDamage: 130 },
 //     { weapon: "Solar Spear", battles: 520, currDamage: 140 },
