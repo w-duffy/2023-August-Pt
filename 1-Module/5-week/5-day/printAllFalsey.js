eb3da1660e658a5289b2053c68091847ad2b8e42
will <w.duffy@outlook.com>
2023-09-22 23:56:47 -0500

    update

---
+++ 1-Module/5-week/5-day/printAllFalsey.js
@@ -0,0 +1,51 @@
+/***********************************************************************
+You are given a recursive function, `printAllFalsy()`, that iterates t
+through the TRUTHY_FALSEY array.
+
+The function should prints all the falsy values it encounters.
+
+The argument for the function will be the index to begin looking.
+You only need to check indicies that're greater than or equal to the argument.
+
+Examples:
+
+const TRUTHY_FALSEY = [true, false, 1, 0, "hello", "", "world", null, undefined, NaN];
+
+printAllFalsy(0);
+// false
+// 0
+// "" //(looks blank on vscode)
+// null
+// undefined
+// NaN
+
+printAllFalsy(2);
+// 0
+// "" //(looks blank on vscode)
+// null
+// undefined
+// NaN
+
+***********************************************************************/
+
+const TRUTHY_FALSEY = [true, false, 1, 0, "hello", "", "world", null, undefined, NaN];
+
+function printAllFalsy(index) {
+
+}
+
+
+printAllFalsy(0);
+// false
+// 0
+// "" //(looks blank on vscode)
+// null
+// undefined
+// NaN
+
+printAllFalsy(2);
+// 0
+// "" //(looks blank on vscode)
+// null
+// undefined
+// NaN
